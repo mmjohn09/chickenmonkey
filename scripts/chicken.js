eb682ee116e6a5d7f09a99651473042393860f46
Maggie Johnson <maggie@Maggies-MacBook-Pro.local>
2019-07-15 15:54:20 -0500

    added code for final instruction of exercise

--- scripts/chicken.js
+++ scripts/chicken.js
@@ -6,11 +6,15 @@
 // }
 
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-    if (currentNumber % 5 === 0) {
-        console.log("chicken")
+
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("chickenmonkey")
     }
     else if (currentNumber % 7 === 0) {
         console.log("monkey")
+    }
+    else if (currentNumber % 5 === 0) {
+        console.log("chicken")
     }
     else console.log(currentNumber)
 }
